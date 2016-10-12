cf1c951b13674f00edc9dbd8af98110f64cefe06
avasylenko <yogik4836@mail.ru>
2016-10-12 17:53:11 +0300

    First commit 2

---
+++ src/main/java/ru/itsphere/app/Main.java
@@ -0,0 +1,13 @@
+package ru.itsphere.app;
+
+/**
+ * Created by avasylenko on 10/12/2016.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hellowwaaa World ");
+    }
+
+}
+
