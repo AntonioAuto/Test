caf8fb73c0fc79eb05adf95f4748f0a6b18d3052
avasylenko <yogik4836@mail.ru>
2016-10-12 17:59:47 +0300

    First commit 3

--- src/main/java/ru/itsphere/app/Main.java
+++ src/main/java/ru/itsphere/app/Main.java
@@ -6,7 +6,7 @@ package ru.itsphere.app;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hellowwaaa World ");
+        System.out.println("Hellowwaasa Worldaaa ");
     }
 
 }
